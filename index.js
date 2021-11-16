81772174a6499c4c82ce4821e3209c78a4da473a
ArtyomSukiasyan <artiom.suqiasyan1401@gmail.com>
2021-11-16 12:34:02 +0400

    fix: finished timer

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const result = document.querySelector("#result");
 const userInputHours = document.querySelector("#hours");
 const userInputMinutes = document.querySelector("#minutes");
 const userInputSeconds = document.querySelector("#seconds");
+let id;
 
-function start() {
-  let hours = Number(userInputHours.value);
-  let minutes = Number(userInputMinutes.value);
-  let seconds = Number(userInputSeconds.value);
+function getValues() {
+  const hours = Number(userInputHours.value);
+  const minutes = Number(userInputMinutes.value);
+  const seconds = Number(userInputSeconds.value);
+
+  return [hours, minutes, seconds];
+}
+
+function validation() {
+  const [hours, minutes, seconds] = getValues();
+
+  return seconds < 0 || minutes < 0 || hours < 0;
+}
+
+function fixInputs() {
+  let [hours, minutes, seconds] = getValues();
 
   if (seconds > 59) {
     const over = Math.floor(seconds / 60);
@@ -22,7 +35,63 @@ function start() {
     hours += over;
     minutes = minutes - over * 60;
   }
+
+  return [hours, minutes, seconds];
+}
+
+function startTimer() {
+  if (validation()) {
+    alert("Please, enter a correct value");
+    return;
+  }
+
+  startBtn.disabled = true;
+
+  const [hours, minutes, seconds] = fixInputs();
+
+  result.textContent = `${hours} ${minutes} ${seconds}`;
+  id = setInterval(startCalc, 1000);
+}
+
+function startCalc() {
+  const content = result.textContent.split(" ");
+  let hours = content[0];
+  let minutes = content[1];
+  let seconds = content[2];
+
+  if (hours === 0 && minutes === 0 && seconds === 0) {
+    clearInterval(id);
+    return;
+  }
+
+  if (seconds === 0) {
+    minutes--;
+    seconds = 60;
+  }
+
+  if (minutes === -1) {
+    hours--;
+    minutes = 59;
+  }
+  
+  seconds--;
+
   result.textContent = `${hours} ${minutes} ${seconds}`;
 }
 
-startBtn.addEventListener("click", start);
+function stopTimer() {
+  continueBtn.disabled = false;
+  clearInterval(id);
+}
+
+function continueTimer() {
+  continueBtn.disabled = true;
+
+  id = setInterval(startCalc, 1000);
+}
+
+startBtn.addEventListener("click", startTimer);
+
+stopBtn.addEventListener("click", stopTimer);
+
+continueBtn.addEventListener("click", continueTimer);
